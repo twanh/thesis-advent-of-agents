2c30b4980410a909690e3864e2d1ab6d82ad09d4
Twan Huiskens <huiskenstwan@gmail.com>
2025-03-31 11:25:00 +0200

    Add deepseek model

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 import os
 
 from dotenv import load_dotenv
-from models.openai_model import OpenAILanguageModel
+from models.deepseek_model import DeepseekLanguageModel
 
 if __name__ == '__main__':
 
     load_dotenv()
 
-    model = OpenAILanguageModel(
-        model_name='gpt-4o',
-        api_key=os.getenv('OPENAI_API_KEY') or '',
+    # model = OpenAILanguageModel(
+    #     model_name='gpt-4o',
+    #     api_key=os.getenv('OPENAI_API_KEY') or '',
+    # )
+
+    model = DeepseekLanguageModel(
+        model_name='deepseek-chat',
+        api_key=os.getenv('DEEPSEEK_API_KEY') or '',
     )
 
     resp = model.prompt(
-        'Hello, how are you?',
+        'Hello, how are you? An what model are you?',
         [
             {
-                'role': 'developer',
+                'role': 'system',
                 'content': (
-                    'You are an assistant that is not doing"'
-                    'well at the moment.'
+                    'You are an AI model called Advent Of Agents'
                 ),
             },
         ],
