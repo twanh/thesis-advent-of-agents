13e227b07a549dcd089aef5935512f43b085970a
Twan Huiskens <huiskenstwan@gmail.com>
2025-04-28 15:44:38 +0200

    Add retreival_rank_solutions prompt to PROMPTS

--- src/prompts/prompts.py
+++ src/prompts/prompts.py
@@ -15,4 +15,5 @@ def _load_prompt_from_file(prompt_name: str) -> str:
 PROMPTS = {
     'test': _load_prompt_from_file('test'),
     'pre_processing': _load_prompt_from_file('pre_processing'),
+    'retreival_rank_solutions': _load_prompt_from_file('retreival_rank_solutions'),  # noqa: E501
 }
